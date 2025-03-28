4627bbd4c931b3e5005b3004dff029922bb677ff
Paul Mollahan <paul.mollahan.digirati@gmail.com>
2025-03-28 14:11:37 +0000

    Update vocs.config.ts

--- vocs.config.ts
+++ vocs.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	logoUrl: "/tanc-logo.png",
 	topNav: [
 		{ text: "Workshop", link: "/getting-started", match: "/" },
-		{ text: "Customer Portal", link: "https://google.com" },
+		{ text: "Customer Portal", link: "https://management.dlcs-trial.digirati.io/" },
 	],
 	theme: {
 		accentColor: {
